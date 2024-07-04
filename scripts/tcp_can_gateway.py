56a50218648627b7f8f2256576e6bef0d00d40f8
Victor Belov <belovictor@gmail.com>
2024-07-04 23:09:53 +0300

    Fix bug with launch parameter for port

--- scripts/tcp_can_gateway.py
+++ scripts/tcp_can_gateway.py
@@ -5,10 +5,10 @@ from can2tcp.can2tcp import SocketCANGateway, TCPCanGateway
 
 if __name__ == "__main__":
     rospy.init_node('tcp_can_gateway')
-    rospy.loginfo("TCP CAN Gateway starting")
     can_interface = rospy.get_param('~can_interface', 'vcan0')
     gateway_host = rospy.get_param('~gateway_host', '192.168.1.4')
-    gateway_port = 20001
+    gateway_port = rospy.get_param('~gateway_port', 20001)
+    rospy.loginfo("TCP CAN Gateway starting on interface %s for host %s:%d", can_interface, gateway_host, gateway_port)
     tcp_can_gateway = TCPCanGateway(gateway_host, gateway_port)
     socket_can_gateway = SocketCANGateway(interface=can_interface, bitrate=1000000)
     tcp_can_gateway.set_receive_callback(socket_can_gateway.send)
